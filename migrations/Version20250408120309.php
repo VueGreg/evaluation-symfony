690e6400d36d92ede40756728485ed06bfc680be
Gregory Wolff <gregory.wolff.dev@gmail.com>
2025-04-08 15:53:19 +0200

    Rendu d'évaluation

---
+++ migrations/Version20250408120309.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250408120309 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE TABLE task (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, assigned_user_id INTEGER DEFAULT NULL, title VARCHAR(255) NOT NULL, description CLOB DEFAULT NULL, status CLOB NOT NULL --(DC2Type:json)
+            , created_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
+            , CONSTRAINT FK_527EDB25ADF66B1A FOREIGN KEY (assigned_user_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE)
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE INDEX IDX_527EDB25ADF66B1A ON task (assigned_user_id)
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE "user" (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles CLOB NOT NULL --(DC2Type:json)
+            , password VARCHAR(255) NOT NULL)
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL ON "user" (email)
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE messenger_messages (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, body CLOB NOT NULL, headers CLOB NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
+            , available_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
+            , delivered_at DATETIME DEFAULT NULL --(DC2Type:datetime_immutable)
+            )
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE INDEX IDX_75EA56E0FB7336F0 ON messenger_messages (queue_name)
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE INDEX IDX_75EA56E0E3BD61CE ON messenger_messages (available_at)
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE INDEX IDX_75EA56E016BA31DB ON messenger_messages (delivered_at)
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            DROP TABLE task
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE "user"
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE messenger_messages
+        SQL);
+    }
+}
---
+++ src/Controller/TaskController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Task;
+use App\Repository\TaskRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Nelmio\ApiDocBundle\Attribute\Model;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use OpenApi\Attributes as OA;
+
+#[OA\Response(
+    response: 200,
+    description: 'Returns the api',
+    content: new OA\JsonContent(
+        type: 'array',
+        items: new OA\Items(ref: new Model(type: Task::class, groups: ['full']))
+    )
+)]
+
+#[Route('/api/task')]
+final class TaskController extends AbstractController
+{
+    #[OA\Tag(name: 'Tasks')]
+    #[Route('', name: 'api_task_index', methods: ['GET'])]
+    public function index(TaskRepository $taskRepository, SerializerInterface $serializer): JsonResponse
+    {
+        $tasks = $taskRepository->findAll();
+        $data = $serializer->serialize($tasks, 'json', ['groups' => 'task:read']);
+
+        return new JsonResponse($data, Response::HTTP_OK, [], true);
+    }
+
+    #[OA\Tag(name: 'Tasks')]
+    #[Route('', name: 'api_task_new', methods: ['POST'])]
+    #[OA\RequestBody(
+        required: true,
+        content: new OA\JsonContent(
+            required: ['title', 'description'],
+            properties: [
+                new OA\Property(property: 'title', type: 'string', description: 'Title of the Task'),
+                new OA\Property(property: 'description', type: 'string', description: 'Description of the Task'),
+            ]
+        )
+    )]
+    public function new(Request $request, EntityManagerInterface $entityManager, SerializerInterface $serializer): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        if (!$data) {
+            return new JsonResponse(['error' => 'Invalid JSON'], Response::HTTP_BAD_REQUEST);
+        }
+
+        $task = new Task();
+        $task->setTitle($data['title']);
+        $task->setDescription($data['description']);
+
+        $entityManager->persist($task);
+        $entityManager->flush();
+
+        $taskData = $serializer->serialize($task, 'json', ['groups' => 'task:read']);
+
+        return new JsonResponse($taskData, Response::HTTP_CREATED, [], true);
+    }
+
+    #[OA\Tag(name: 'Tasks')]
+    #[Route('/{id}', name: 'api_task_show', methods: ['GET'])]
+    public function show(Task $task, SerializerInterface $serializer): JsonResponse
+    {
+        $data = $serializer->serialize($task, 'json', ['groups' => 'task:read']);
+
+        return new JsonResponse($data, Response::HTTP_OK, [], true);
+    }
+
+    #[OA\Tag(name: 'Tasks')]
+    #[Route('/{id}', name: 'api_task_edit', methods: ['PUT'])]
+    public function edit(Request $request, Task $task, EntityManagerInterface $entityManager, SerializerInterface $serializer): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        if (!$data) {
+            return new JsonResponse(['error' => 'Invalid JSON'], Response::HTTP_BAD_REQUEST);
+        }
+
+        $task->setTitle($data['title']);
+        $task->setDescription($data['description']);
+
+        $entityManager->flush();
+        $taskData = $serializer->serialize($task, 'json', ['groups' => 'task:read']);
+
+        return new JsonResponse($taskData, Response::HTTP_OK, [], true);
+    }
+
+    #[OA\Tag(name: 'Tasks')]
+    #[Route('/{id}', name: 'api_task_delete', methods: ['DELETE'])]
+    public function delete(Task $task, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $entityManager->remove($task);
+        $entityManager->flush();
+
+        return new JsonResponse(['status' => 'Task deleted'], Response::HTTP_NO_CONTENT);
+    }
+}
